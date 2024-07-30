90634739244780a77ae985fa759778b1f753261c
Fouza <gf_oukacha@esi.dz>
2024-07-30 22:29:15 +0100

    router + test send request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,49 @@
 import React from 'react'
 import axios from 'axios'
 import CONFIG from '../config.json'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './global.css'
+
+//To use fontawesome icons do this in terminal :
+//npm i @fortawesome/react-fontawesome
+//AND
+//npm i @fortawesome/free-solid-svg-icons
+// link : https://www.npmjs.com/package/@fortawesome/react-fontawesome
+// link :
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products: []
+        }
+    }
 
 
     async componentDidMount() {
-        await axios.get(`${CONFIG.api_server}/api/product/prod?price=${25}`).then(res => {
-            console.log(res.data)
-        })
+        setTimeout(async () => { // Set Time Out just to demonstrate the loading
+            await axios.get(`${CONFIG.api_server}/api/product/prod?price=${25}`).then(response => {
+                console.log(response)
+                this.setState({ products: response.data })
+            })
+        }, 1000)
     }
 
     render() {
+        const { products } = this.state
         return (
-            <div>Hoooome</div>
+            <div>
+                <h2>Hoooome</h2>
+
+                {products && products.length > 0 ? <ul>
+                    {products.map(product => (
+                        <li key={product._id}>{product.name}</li>
+                    ))}
+                </ul> :
+                    <FontAwesomeIcon icon={faSpinner} className='fa-spin spin_icon' color='#000' />
+                }
+            </div>
         )
     }
 
