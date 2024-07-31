b5988355d745dece728d949bfafdfe570939ee73
Fouza <gf_oukacha@esi.dz>
2024-07-31 10:13:22 +0100

    timeout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,32 +16,47 @@ class Home extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            products: []
+            products: [],
+            error: null
         }
     }
 
 
     async componentDidMount() {
-        setTimeout(async () => { // Set Time Out just to demonstrate the loading
-            await axios.get(`${CONFIG.api_server}/api/product/prod?price=${25}`).then(response => {
+        try {
+            // setTimeout(async () => { // Set Time Out just to demonstrate the loading
+            await axios.get(`${CONFIG.api_server}/api/product/prod?price=${25}`, { timeout: 1000 }).then(response => {
                 console.log(response)
-                this.setState({ products: response.data })
+                if (response.data.status === 200) {
+                    this.setState({ products: response.data.products })
+                } else {
+                    this.setState({ error: 'No response from server' })
+                }
             })
-        }, 1000)
+            // }, 1000)
+        } catch (e) {
+            if (e.code === 'ECONNABORTED') {
+                this.setState({ error: 'Request took too long please refresh' })
+            }
+
+        }
     }
 
     render() {
-        const { products } = this.state
+        const { products, error } = this.state
         return (
             <div>
                 <h2>Hoooome</h2>
+                <FontAwesomeIcon icon={faSpinner} className='fa-spin spin_icon' color='#000' />
 
-                {products && products.length > 0 ? <ul>
+                {!error && products && products.length > 0 ? <ul>
                     {products.map(product => (
                         <li key={product._id}>{product.name}</li>
                     ))}
-                </ul> :
+                </ul> : !error ?
                     <FontAwesomeIcon icon={faSpinner} className='fa-spin spin_icon' color='#000' />
+                    :
+                    <p>{error}</p>
                 }
             </div>
         )
